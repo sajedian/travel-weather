f443bd45b9c09171e528776b7935b347cd86db77
Renee Sajedian <rsajedian@gmail.com>
2020-01-20 13:51:20 -0500

    basic design for weather cell, initialized table view controller

---
+++ travel-weather/DayWeatherCell.swift
@@ -0,0 +1,19 @@
+//
+//  DayWeatherCell.swift
+//  Travel Weather
+//
+//  Created by Renee Sajedian on 1/20/20.
+//  Copyright © 2020 Renee Sajedian. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+    
+    //MARK:- Outlets
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+}
---
+++ travel-weather/WeatherListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherListViewController.swift
+//  Travel Weather
+//
+//  Created by Renee Sajedian on 1/19/20.
+//  Copyright © 2020 Renee Sajedian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cellNib = UINib(nibName: "DayWeatherCell", bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: "DayWeatherCell")
+    }
+    
+    //MARK:- Table View Data Source
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DayWeatherCell", for: indexPath)
+        return cell
+    }
+}
