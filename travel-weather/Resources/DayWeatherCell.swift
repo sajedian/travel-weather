380da42845d6a0ffc387204dbf46ffdac8d68100
Renee Sajedian <rsajedian@gmail.com>
2020-01-21 22:18:11 -0500

    updated table cell design to include rounded edges and spacing

--- travel-weather/Resources/DayWeatherCell.swift
+++ travel-weather/Resources/DayWeatherCell.swift
@@ -15,23 +15,28 @@ class DayWeatherCell: UITableViewCell {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var colorView: UIView!
+    
+    
     
     override func awakeFromNib() {
          super.awakeFromNib()
-         let background = UIView(frame: CGRect.zero)
-         background.backgroundColor = UIColor(red: 20/255,
-             green: 160/255, blue: 160/255, alpha: 0.5)
-         backgroundView = background
+         
        }
+    
     
     func configureCell(dayWeather: DayWeather, color: UIColor) {
         dayLabel.text = dayWeather.day.rawValue
         cityLabel.text = dayWeather.city
         tempLabel.text = dayWeather.tempDisplay
         weatherImageView.image = UIImage(systemName: dayWeather.weatherSummary.rawValue)
-        let background = UIView(frame: CGRect.zero)
-        background.backgroundColor = color
-        backgroundView = background
+        colorView.backgroundColor = color
+        colorView.layer.cornerRadius = 10
+//        let background = UIView(frame: CGRect.zero)
+//        background.backgroundColor = color
+//        backgroundView = background
+//        backgroundView?.layer.cornerRadius = 10
+        
     }
     
 }
